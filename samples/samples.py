2abac8ed2465a09b4c23824aeff285320fe3e4f1
Tim Savage <tim@savage.company>
2022-05-30 23:47:31 +1000

    Move samples into samples folder

---
+++ samples/samples.py
@@ -0,0 +1,65 @@
+import logging
+from typing import Sequence
+
+logging.basicConfig(level=logging.DEBUG)
+
+import pyapp_flow as flow
+
+
+@flow.step(outputs=[("options", Sequence[str])])
+def load_options() -> Sequence["str"]:
+    """
+    Load options from file
+    """
+    return ["abc", "def", "ghi"]
+
+
+@flow.step
+def process_option(current_option: str, foo: str):
+    """
+    Process an option
+    """
+    print(foo, current_option)
+
+
+@flow.step
+def flip_option(context: flow.WorkflowContext, current_option: str):
+    """
+    Process an option
+    """
+    context.state["rcurrent_object"] = "".join(reversed(current_option))
+
+
+@flow.step
+def print_flip_option(rcurrent_object: str):
+    """
+    Process an option
+    """
+    print(rcurrent_object)
+
+
+@flow.step(ignore_exceptions=ValueError)
+def fail_nicely():
+    raise ValueError("Eek!")
+
+
+flip_workflow = flow.Workflow(name="Flip Workflow").nodes(
+    flip_option,
+    print_flip_option,
+)
+
+
+process_workflow = (
+    flow.Workflow(name="Do Process")
+    .set_vars(foo="bar")
+    .nodes(
+        fail_nicely,
+        load_options,
+        flow.Conditional("good").true().false(flow.log_message("{foo} is False")),
+    )
+    .foreach("current_option", "options", process_option, flip_workflow)
+)
+
+
+if __name__ == "__main__":
+    process_workflow.execute()
