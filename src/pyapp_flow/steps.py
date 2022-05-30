3c44faee1e577938c03cd1fdb05595f91f7fe467
Tim Savage <tim@savage.company>
2022-05-30 16:48:10 +1000

    Migrate all steps into steps.py, add conditional

--- src/pyapp_flow/steps.py
+++ src/pyapp_flow/steps.py
@@ -1,4 +1,4 @@
-from typing import Callable, Sequence, Union, Tuple, Iterable
+from typing import Callable, Sequence, Union, Tuple, Iterable, Type
 
 from pyapp_flow import extract_inputs, WorkflowContext
 
@@ -33,17 +33,26 @@ class Step:
         output(s) of the step function.
     """
 
-    __slots__ = ("func", "inputs", "outputs", "name", "context_var")
+    __slots__ = (
+        "func",
+        "inputs",
+        "outputs",
+        "name",
+        "ignore_exceptions",
+        "context_var",
+    )
 
     def __init__(
         self,
         func: Callable,
         name: str = None,
         outputs: Sequence[Variable] = None,
+        ignore_exceptions: Union[Type[Exception], Sequence[Type[Exception]]] = None,
     ):
         self.func = func
         self.outputs = outputs or ()
         self.name = name or func.__name__.replace("_", " ")
+        self.ignore_exceptions = ignore_exceptions
 
         self.inputs, self.context_var = extract_inputs(func)
 
@@ -56,20 +65,54 @@ class Step:
         if self.context_var:
             kwargs[self.context_var] = context
 
-        context.info("- Calling `%s`", self)
+        context.info("⚫ Step `%s`", self)
         try:
             results = self.func(**kwargs)
+
         except Exception as ex:
-            context.error("Exception raised in step `%s`: %s", self, ex)
-            raise
+            if self.ignore_exceptions and isinstance(ex, self.ignore_exceptions):
+                context.warning("  ❌ Ignoring exception: %s", ex)
+            else:
+                context.error("  ⛔ Exception raised: %s", ex)
+                raise
 
-        if self.outputs:
-            # Helper so a simple return can be used for a single result
-            if len(self.outputs) == 1:
-                results = (results,)
+        else:
+            if self.outputs:
+                # Helper so a simple return can be used for a single result
+                if len(self.outputs) == 1:
+                    results = (results,)
 
-            for name, value in zip([name for name, _ in self.outputs], results):
-                context.state[name] = value
+                for name, value in zip([name for name, _ in self.outputs], results):
+                    context.state[name] = value
+
+
+def step(
+    func=None,
+    *,
+    name: str = None,
+    outputs: Sequence[Variable] = None,
+    ignore_exceptions: Union[Type[Exception], Sequence[Type[Exception]]] = None,
+) -> Step:
+    """
+    Decorate a method turning it into a step
+    """
+
+    def decorator(func_):
+        return Step(func_, name, outputs, ignore_exceptions)
+
+    return decorator(func) if func else decorator
+
+
+def set_var(**kwargs):
+    """
+    Set context variable to specified values
+    """
+
+    @step(name="set var")
+    def _set_var(context: WorkflowContext):
+        context.state.update(**kwargs)
+
+    return _set_var
 
 
 class ForEach:
@@ -85,7 +128,7 @@ class ForEach:
         self._nodes = nodes
 
     def __call__(self, context: WorkflowContext):
-        context.info("- For `%s` in `%s`", self.target_var, self.in_var)
+        context.info("➰ For `%s` in `%s`", self.target_var, self.in_var)
         try:
             iterable = context.state[self.in_var]
         except KeyError:
@@ -111,13 +154,13 @@ class CaptureErrors:
 
     __slots__ = ("target_var", "_nodes", "try_all")
 
-    def __init__(self, target_var: str, *nodes: Callable, try_all: bool = False):
+    def __init__(self, target_var: str, *nodes: Callable, try_all: bool = True):
         self.target_var = target_var
         self._nodes = nodes
         self.try_all = try_all
 
     def __call__(self, context: WorkflowContext):
-        context.info("Capture errors into `%s`", self.target_var)
+        context.info("🥅 Capture errors into `%s`", self.target_var)
 
         context.state.setdefault(self.target_var, list())
         with context:
@@ -131,3 +174,42 @@ class CaptureErrors:
 
     def __str__(self):
         return f"Capture errors into `{self.target_var}`"
+
+
+class Conditional:
+    """
+    Branch a workflow based on a condition.
+
+    A condition can be either a boolean based context variable or a callable that accepts a workflow context.
+    """
+
+    __slots__ = ("condition", "_true_nodes", "_false_nodes")
+
+    def __init__(
+        self, condition: Union[str, Callable[[WorkflowContext], bool]], *nodes: Callable
+    ):
+        if isinstance(condition, str):
+            self.condition = lambda context: bool(context.state.get(condition))
+        elif callable(condition):
+            self.condition = condition
+        else:
+            raise TypeError("condition not context variable name or callable")
+
+        self._true_nodes = nodes
+        self._false_nodes = None
+
+    def __call__(self, context: WorkflowContext):
+        condition = self.condition(context)
+        context.info("Condition is %s errors into `%s`", condition)
+
+        nodes = self._true_nodes if condition else self._false_nodes
+        if nodes:
+            with context:
+                for node in nodes:
+                    node(context)
+
+    def if_false(self, *nodes: Callable):
+        """
+        If the condition is false execute these nodes
+        """
+        self._false_nodes = nodes
