42a508e4b9ac63873a6c9798a46c298455cb37a0
Tim Savage <tim@savage.company>
2023-06-30 01:14:54 +1000

    Update how module is imported

--- src/pyapp_flow/cli/actions.py
+++ src/pyapp_flow/cli/actions.py
@@ -1,23 +1,38 @@
-import importlib
+import importlib.util
 import logging
+import sys
 from pathlib import Path
 from typing import Dict
+from types import ModuleType
 
 from pyapp_flow import Workflow
 
 log = logging.getLogger(__package__)
 
 
-def _import_flow_file(flow_file: Path) -> object:
-    """Import flow file and return the module."""
+def _import_flow_file(flow_file: Path) -> ModuleType:
+    """Import flow file and return the module.
+
+    Based off the import code in nox.
+    """
     if not flow_file.is_file():
         raise RuntimeError(f"Flow file not found at: {flow_file}")
 
-    flow_module = importlib.import_module(flow_file.stem, flow_file.parent)
-    return flow_module
+    spec = importlib.util.spec_from_file_location("user_flow_module", flow_file)
+    if spec is None:
+        raise RuntimeError(f"Unable to import flow file: {flow_file}")
+
+    module = importlib.util.module_from_spec(spec)
+    sys.modules["user_flow_module"] = module
+
+    # See https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+    loader = spec.loader
+    loader.exec_module(module)
+
+    return module
 
 
-def _resolve_flow(module: object, name: str) -> Workflow:
+def _resolve_flow(module: ModuleType, name: str) -> Workflow:
     """Resolve a workflow from a module."""
     flows = {
         value.name: value
@@ -30,7 +45,7 @@ def _resolve_flow(module: object, name: str) -> Workflow:
     except KeyError:
         raise RuntimeError(
             f"Workflow not found: {name}; try one of: {', '.join(flows)}"
-        )
+        ) from None
 
 
 def _resolve_required_args(flow: Workflow) -> Dict[str, type]:
