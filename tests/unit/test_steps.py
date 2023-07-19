fd572460cea7e3af6c634e95bea865404bf0b1e0
Tim Savage <tim@savage.company>
2023-07-19 22:23:31 +1000

    Test for fatal step

--- tests/unit/test_steps.py
+++ tests/unit/test_steps.py
@@ -1,7 +1,7 @@
 import pytest
 
 from pyapp_flow import steps
-from pyapp_flow.errors import StepFailedError
+from pyapp_flow.errors import StepFailedError, FatalError
 from pyapp_flow.testing import call_node
 
 
@@ -9,4 +9,11 @@ def test_failed():
     target = steps.failed("Failed as {my_var} is not set")
 
     with pytest.raises(StepFailedError, match="Failed as test is not set"):
+        call_node(target, my_var="test")
+
+
+def test_fatal():
+    target = steps.fatal("Fatal error as {my_var} is not set")
+
+    with pytest.raises(FatalError, match="Fatal error as test is not set"):
         call_node(target, my_var="test")
