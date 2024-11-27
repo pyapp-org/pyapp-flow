289aa21bb50eb4598dba64e5cd945da63a82a885
Tim Savage <tim@savage.company>
2024-11-27 12:19:36 +1100

    Add helper for masking dictionary values for sensitive keys

--- src/pyapp_flow/helpers.py
+++ src/pyapp_flow/helpers.py
@@ -2,11 +2,11 @@
 
 import logging
 from contextlib import contextmanager
-from typing import Union
+from typing import Any
 
 
 @contextmanager
-def change_log_level(level: Union[int, str, None], *, logger: logging.Logger = None):
+def change_log_level(level: int | str | None, *, logger: logging.Logger | None = None):
     """Temporarily change the log level of a logger."""
     if level is None:
         yield
@@ -29,3 +29,25 @@ def human_join_strings(items, *, conjunction: str = "and", empty: str = ""):
         return items[0]
 
     return f"{', '.join(items[:-1])} {conjunction} {items[-1]}"
+
+
+SENSITIVE_WORDS = ("credentials", "authorization", "token", "secret", "password")
+
+
+def set_sensitive_words(sensitive_words: tuple[str, ...]):
+    global SENSITIVE_WORDS
+    SENSITIVE_WORDS = sensitive_words
+
+
+def mask_keys(d: dict[str, Any], *, sensitive_words: tuple[str, ...] | None = None):
+    """Mask dictionary values for keys that contain a sensitive name"""
+
+    sensitive_words = sensitive_words or SENSITIVE_WORDS
+
+    def _mask(key: str, value: Any) -> Any:
+        if any(word in key for word in sensitive_words):
+            return "****"
+        return value
+
+    return {key: _mask(key, value) for key, value in d.items()}
+
--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -16,3 +16,22 @@ def test_human_join_strings(items, expected):
     actual = helpers.human_join_strings(items)
 
     assert actual == expected
+
+
+def test_mask_keys():
+    sample = {
+        "foo": "bar",
+        "username": "dave",
+        "password": "secret-password",
+        "account-secret": "secret-details"
+    }
+
+    actual = helpers.mask_keys(sample)
+
+    assert actual == {
+        "foo": "bar",
+        "username": "dave",
+        "password": "****",
+        "account-secret": "****",
+    }
+
